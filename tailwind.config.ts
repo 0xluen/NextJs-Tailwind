7a2cb2a8b2f67ff34ca17832c8aeff7b80283c0f
Luen <luen.eth@gmail.com>
2023-10-16 22:33:58 +0000

    Revert "components"
    This reverts commit 5d246357b870fc0d668e98d1bffa052d8bdbbfd1.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,34 +6,10 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#1c4f82",
-
-          secondary: "#7d919b",
-
-          accent: "#eb6b47",
-
-          neutral: "#23282f",
-
-          dark: "#212121",
-
-          info: "#0092d6",
-
-          success: "#6cb288",
-
-          warning: "#daad58",
-
-          error: "#ab3d30",
-        },
-      },
-    ],
-  },
   theme: {
-    extend: {},
+    extend: {
+    },
   },
-  plugins: [require("daisyui")]
+  plugins: [],
 }
 export default config
