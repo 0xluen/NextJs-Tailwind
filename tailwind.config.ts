5d246357b870fc0d668e98d1bffa052d8bdbbfd1
Luen <luen.eth@gmail.com>
2023-10-16 22:31:45 +0000

    components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,10 +6,34 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-    },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#1c4f82",
+
+          secondary: "#7d919b",
+
+          accent: "#eb6b47",
+
+          neutral: "#23282f",
+
+          dark: "#212121",
+
+          info: "#0092d6",
+
+          success: "#6cb288",
+
+          warning: "#daad58",
+
+          error: "#ab3d30",
+        },
+      },
+    ],
   },
-  plugins: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")]
 }
 export default config
